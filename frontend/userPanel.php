32b4e9a1bdc4fa93f6550e19ccb6ebbe2a27c573
WesleyPutman <barryvert@gmail.com>
2025-02-28 09:20:46 +0100

    Refactor CSS styles and add global font family no php yet

---
+++ frontend/userPanel.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="frontend/assets/css/style.css">
+</head>
+<body>
+<!--Insert here the future header php with include-->
+
+<div class="mainContainer flex justify-between">
+    <div class="leftUserContainer shadow-lg rounded-3xl p-10">
+        <h1>Mon compte</h1>
+        <ul class="flex">
+            <li><a href="#">Mon profil</a></li><hr>
+            <li><a href="#">Mes engagements</a></li><hr>
+            <li><a href="#">Statistiques</a></li><hr>
+            <li><a href="#">Certifications</a></li><hr>
+            <li><a href="#">Messagerie</a></li><hr>
+            <li><a href="#">Paramètres</a></li><hr>
+        </ul>
+    </div>
+    <div class="rightUserContainer">
+
+    </div>
+</div>
+
+</div>
+</body>
+</html>
