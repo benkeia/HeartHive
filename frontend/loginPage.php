c3d2cd88161d98ed495016c4078cbfec4ddfeb18
Baptiste SAEGAERT <baptiste.saegaert@gmail.com>
2025-03-13 09:15:49 +0100

    Ajout de la page de connexion avec un nouveau style CSS et mise à jour du fichier .gitignore

--- frontend/loginPage.php
+++ frontend/loginPage.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylelogin.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Page de connexion</title>
 </head>
+
 <body>
-<?php
-include '../backend/db.php';
-?>
-<div id="container">
-    <h1>Connexion au site</h1>
-    <form action="login.php" method="post">
-        <label for="mail">Entrez votre email :</label>
-        <input type="email" id="mail" name="mail" placeholder="mail" required>
-        <br>
-        <label for="password">Entrez votre mot de passe :</label>
-        <input type="password" id="password" name="password" placeholder="mot de passe" required>
-        <br>
-        <input type="submit" value="Valider">
-    </form>
-</div>
+    <?php
+    include '../backend/db.php';
+    ?>
+    <div id="container">
+        <h1>Connexion au site</h1>
+        <form action="login.php" method="post" class="form">
+            <label for="mail">Entrez votre email :</label>
+            <input type="email" id="mail" name="mail" placeholder="mail" required>
+            <label for="password">Entrez votre mot de passe :</label>
+            <input type="password" id="password" name="password" placeholder="mot de passe" required>
+            <input type="submit" value="Valider" class="btn">
+        </form>
+    </div>
 
 </body>
+
 </html>
