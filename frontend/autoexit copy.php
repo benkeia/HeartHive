b681cff09f00ae7612414f6b4aee9c365db60380
Baptiste SAEGAERT <baptiste.saegaert@gmail.com>
2025-03-20 09:23:07 +0100

    Suppression des fichiers img.txt et autoexit copy.php

--- frontend/autoexit copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-/*$currentPage= $_SERVER['SCRIPT_NAME'];
-echo $currentPage;*/
-if ($_SESSION['authentification'] == false) {
-    header('Location: landingpage.php');
-    exit();
-}
